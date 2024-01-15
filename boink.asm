22063934c085778f739af5aa0fbf61a138ed774a
ragerin <ragerin@gmail.com>
2024-01-15 21:31:10 +0100

    added wall bounces and ball moving

--- boink.asm
+++ boink.asm
@@ -47,19 +47,86 @@
     CP A, 1
     CALLR Z, .MovePaddleARight
 
+    ; Update the ball position
+    CALLR .UpdateBallX
+    CALLR .CheckBallXCollision
+
     ; Render objects
-    CALL .DrawPaddles
+    CALLR .DrawGameOjbects
 
     RET
 
-.DrawPaddles
+
+.CheckXCollision
+    ; BC is the X value to check
+    ; DE is the left limit
+    ; FG is the right limit
+    ; A is 1 if inside, 0 if out
+    CP BC, DE
+    JP LT, .CheckXCollisionOut      ; X is left of left limit
+    CP BC, FG
+    JP GT, .CheckXCollisionOut      ; X is right of right limit
+    LD A, 0x01                      ; ... else we're within
+    RET
+.CheckXCollisionOut
+    LD A, 0x00                      ; ... or we're out
+    RET
+
+
+.DrawGameOjbects
     ; Clear the game objects
     LD A, 0x05                      ; ClearVideoPage
     LD B, 1                         ; Page 1
     LD C, 0x00                      ; Color
     INT 0x01, A                     ; Video interrupt
+    CALLR .DrawPaddles
+    CALLR .DrawBall
+    RET
 
-    ; Draw the paddles
+.DrawBall
+    LD A, 0x06                      ; DrawFilledRectangle
+    LD B, 1                         ; Video page
+    LD CD, (.BallX)                 ; X position top left
+    LD EF, (.BallY)                 ; Y position top left
+    LD GH, (.BallSize)              ; Width
+    LD IJ, (.BallSize)              ; Height
+    LD K, 100                       ; Color
+    INT 0x01, A                     ; Video interrupt
+    RET
+
+.UpdateBallX
+    LD BC, (.BallX)                 ; Load the ball X position
+    LD DE, (.BallDX)                ; Load the ball X delta
+    LD F, (.BallDXS)                ; Load the ball X delta sign
+
+    CP F, 0x00                      ; Check if the sign is 0 positive, or 1 negative
+    CALLR EQ, .UpdateBallX_add      ; Add or sub depending on the sign
+    CALLR NE, .UpdateBallX_sub
+    LD (.BallX), BC                 ; Store the new X position in memory
+    RET
+
+.UpdateBallX_add
+    ADD BC, DE
+    RET
+.UpdateBallX_sub
+    SUB BC, DE
+    RET
+
+.CheckBallXCollision
+    LD BC, (.BallX)                 ; Load the ball X position
+    LD DE, 10                       ; Left limit
+    LD FG, 465                      ; Right limit
+    CALLR .CheckXCollision
+    CP A, 0x01
+    JR NE, .InvertBallDXS           ; Ball is out of the field, so invert DXS
+    RET
+.InvertBallDXS
+    LD A, (.BallDXS)
+    XOR A, 1
+    LD (.BallDXS), A
+    RET
+
+.DrawPaddles
     ; Shared
     LD A, 0x06                      ; DrawFilledRectangle
     LD B, 1                         ; Video page
@@ -85,9 +152,8 @@
     ADD BC, DE
     ; TODO: Bounding check
 
-    LD (.PaddleAX), BC
+    LD (.PaddleAX), BC              ; Store the new X
     RET
-
 .MovePaddleALeft
     LD BC, (.PaddleAX)              ; Load the paddle X
     LD DE, (.PaddleASpeed)          ; Load the speed
@@ -95,7 +161,7 @@
     SUB BC, DE
     ; TODO: Bounding check
 
-    LD (.PaddleAX), BC
+    LD (.PaddleAX), BC              ; Store the new X
     RET
 
 
@@ -122,3 +188,20 @@
     #DB 0048
 .PaddleHeight
     #DB 0010
+
+.BallSize
+    #DB 005
+.BallX
+    #DB 00237
+.BallDX
+    #DB 005
+.BallDXS
+    #DB 0x00                        ; sign: 0x00 positive, 0x01 negative
+.BallY
+    #DB 0025
+.BallDY
+    #DB 005
+.BallDYS
+    #DB 0x00                        ; sign: 0x00 positive, 0x01 negative
+
+.WallLeft
