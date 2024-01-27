9ae34fc490a30db15d9ea4bce9c3e79e6206bd3c
Florin Ciuică <florin.ciuica@yahoo.com>
2024-01-27 15:45:14 +0200

    Changed the time granulation and adjusted for a smoother transition.

--- boink.asm
+++ boink.asm
@@ -141,7 +141,7 @@
 
 .UpdateCompPaddle
     ; Make the computer sometimes wait a cycle
-    RAND A, 8                       
+    RAND A, 16                       
     CP A, 2
     JP LTE, .UpdateCompPaddle_ret
 
@@ -425,9 +425,9 @@
 .PaddleBY
     #DB 00250
 .PaddleASpeed
-    #DB 008
+    #DB 004
 .PaddleBSpeed
-    #DB 008
+    #DB 004
 .PaddleASize
     #DB 0048
 .PaddleBSize
@@ -440,13 +440,13 @@
 .BallX
     #DB 00237
 .BallDX
-    #DB 003
+    #DB 001
 .BallDXS
     #DB 0x00                        ; sign: 0x00 positive, 0x01 negative
 .BallY
     #DB 0030
 .BallDY
-    #DB 005
+    #DB 002
 .BallDYS
     #DB 0x00                        ; sign: 0x00 positive, 0x01 negative
 
